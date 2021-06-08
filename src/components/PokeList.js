ad5f34fc76a6f12cc32badf712a3830af791a0db
RosaBF <rosaberned30@gmail.com>
2021-06-08 11:27:45 +0200

    add styles

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Pokemon from './Pokemon';
 
 const PokeList = (props) => {
-  const PokemonsFinalList = props.id.map((pokemon, index) => {
+  const PokemonsFinalList = props.thepokemons.map((pokemon, index) => {
     console.log(index);
     return (
       <li key={index}>
-        <Pokemon pokemon={pokemon} />
+        <Pokemon
+          pokemon={pokemon}
+          name={pokemon.name}
+          types={pokemon.types}
+          urlImage={pokemon.url}
+        />
       </li>
     );
   });
-  return <ul>{PokemonsFinalList}</ul>;
+  return <ul className='pokeCards__final--list'>{PokemonsFinalList}</ul>;
 };
 
 export default PokeList;
